b7f0ade082fddee425ca9efa0114b3c9710333db
lvfulong <775331175@qq.com>
2022-07-22 20:36:25 +0800

    add

---
+++ Conch/source/conch/JSWrapper/LayaWrap/JSPromiseRejectionEvent.cpp
@@ -0,0 +1,66 @@
+﻿/**
+@file			JSPromiseRejectionEvent.cpp
+@brief			
+@author			James
+@version		1.0
+@date			2018_3_31
+*/
+
+#include "JSPromiseRejectionEvent.h"
+#include "../JSInterface/JSInterface.h"
+#include "util/Log.h"
+#include <util/JCMemorySurvey.h>
+#include "../../JCScriptRuntime.h"
+
+namespace laya
+{
+	ADDJSCLSINFO(JSPromiseRejectionEvent, JSObjNode);
+    //------------------------------------------------------------------------------
+    JSPromiseRejectionEvent::JSPromiseRejectionEvent()
+    {
+	    AdjustAmountOfExternalAllocatedMemory(128);
+	    JCMemorySurvey::GetInstance()->newClass( "JSPromiseRejectionEvent",10240,this );
+    }
+    JSPromiseRejectionEvent::~JSPromiseRejectionEvent()
+    {
+		m_pReason.Reset();
+		m_pPromise.Reset();
+	    JCMemorySurvey::GetInstance()->releaseClass( "JSPromiseRejectionEvent",this );
+    }
+    JsValue JSPromiseRejectionEvent::getReason()
+    {
+	    return m_pReason.getJsObj();
+    }
+    JsValue JSPromiseRejectionEvent::getPromise()
+    {
+        return m_pPromise.getJsObj();
+    }
+	void JSPromiseRejectionEvent::setReason(JSValueAsParam pObj)
+	{
+		m_pReason.set(reasonid, this, pObj);
+	}
+	void JSPromiseRejectionEvent::setPromise(JSValueAsParam pObj)
+	{
+		m_pPromise.set(promiseid, this, pObj);
+	}
+	void JSPromiseRejectionEvent::setType(const char* strType)
+	{
+		m_strType = strType;
+	}
+	char* JSPromiseRejectionEvent::getType()
+	{
+		return (char*)m_strType.c_str();
+	}
+    void JSPromiseRejectionEvent::exportJS()
+    {
+        JSP_CLASS("PromiseRejectionEvent", JSPromiseRejectionEvent);
+        JSP_ADD_PROPERTY_RO(reason, JSPromiseRejectionEvent, getReason);
+        JSP_ADD_PROPERTY_RO(promise, JSPromiseRejectionEvent, getPromise);
+        JSP_ADD_PROPERTY_RO(type, JSPromiseRejectionEvent, getType);
+	    JSP_INSTALL_CLASS("PromiseRejectionEvent", JSPromiseRejectionEvent);
+    }
+}
+//------------------------------------------------------------------------------
+
+
+//-----------------------------END FILE--------------------------------
---
+++ Conch/source/conch/JSWrapper/LayaWrap/JSPromiseRejectionEvent.h
@@ -0,0 +1,55 @@
+﻿/**
+@file			JSPromiseRejectionEvent.h
+@brief			
+@author			James
+@version		1.0
+@date			2018_3_31
+*/
+
+#ifndef __JSPromiseRejectionEvent_H__
+#define __JSPromiseRejectionEvent_H__
+
+
+//包含头文件
+#include <stdio.h>
+#include <string>
+#include <map>
+#include "../JSInterface/JSInterface.h"
+#include "../JSInterface/JSObjBase.h"
+
+namespace laya
+{
+	enum { reasonid, promiseid};
+    class JSPromiseRejectionEvent :public JsObjBase, public JSObjNode
+    {
+    public:
+	    static JsObjClassInfo JSCLSINFO;
+	    static void exportJS();
+
+        JSPromiseRejectionEvent();
+
+	    ~JSPromiseRejectionEvent();
+
+		void setReason(JSValueAsParam pObj);
+		
+		JsValue getReason();
+		
+		void setPromise(JSValueAsParam pObj);
+		
+		JsValue getPromise();
+		
+		char* getType();
+		
+		void setType(const char* strType);
+    public:
+		std::string m_strType;
+	    JsObjHandle m_pReason;
+		JsObjHandle m_pPromise;
+    };
+}
+//------------------------------------------------------------------------------
+
+
+#endif //__JSPromiseRejectionEvent_H__
+
+//-----------------------------END FILE--------------------------------
